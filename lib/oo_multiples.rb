8134d7f8938222d4ffed4cc059691f6fc736d42b
Amanda <ajnikrant@gmail.com>
2021-03-11 09:49:07 -0500

    commit tree 18247bca4438f46df1b3c527994d098e0b0ed691

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :limit
+    @@all=[]
+
+    def initialize (limit)
+        @limit=limit
+
+        @@all<<self
+    end
+
+    def collect_multiples
+        (1...limit).to_a.select{|num| num % 3 == 0 || num % 5 == 0 }
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+
+
+
+end
